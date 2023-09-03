03432c40517ac2823295bff6c7bfbf845dfeebf2
Abhinav Singh <thakurabhinav17122001@gmail.com>
2023-09-03 13:52:03 +0530

    Broker chnages done

--- central-broker/consumers/consumer-topic1.go
+++ central-broker/consumers/consumer-topic1.go
@@ -5,11 +5,14 @@ import (
 	"os"
 	"os/signal"
 	"sync"
+	"time"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/geekyabhi/resource-allocater/utils"
 )
 
 var topic = "ywbuiicx-email"
+var temp_signal = make(chan bool)
 
 func NewTopic1Consumer() *utils.KafkaConsumer {
 	consumer, err := utils.NewKafkaConsumer(topic, topic)
@@ -20,49 +23,48 @@ func NewTopic1Consumer() *utils.KafkaConsumer {
 	return consumer
 }
 
-func RunTopic1Consumer(consumer *utils.KafkaConsumer) {
-	// Get the Kafka consumer instance
-	kafkaConsumer := consumer.GetConsumer()
-
-	// Wait for termination signals
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
-
-	var wg sync.WaitGroup
-
-	// Start consuming messages
+func StartConsumer(kafkaConsumer *kafka.Consumer , wg *sync.WaitGroup){
 	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		fmt.Println("Starting Topic 1 Consumer")
+	defer wg.Done()
+	fmt.Println("Starting Topic 1 Consumer")
 
-		for {
-			select {
-			case <-signals:
-				fmt.Println("Received termination signal. Closing Topic 1 consumer...")
-				return
-			default:
-				msg, err := kafkaConsumer.ReadMessage(-1)
-				if err != nil {
-					fmt.Printf("Topic 1 - Error reading message: %v\n", err)
-				} else {
-					fmt.Printf("Topic 1 - Received message: Key: %s, Value: %s\n", string(msg.Key), string(msg.Value))
-					// Process the message here
-				}
+	for {
+		select {
+		case <- temp_signal:
+			fmt.Println("Received termination signal. Closing Topic 1 consumer...")
+			return
+		default:
+			msg, _ := kafkaConsumer.ReadMessage(time.Microsecond)
+			
+			if msg != nil {
+				fmt.Printf("Topic 1 - Received message: Key: %s, Value: %s\n", string(msg.Key), string(msg.Value))
 			}
 		}
-	}()
+	}
+}
 
-	// Wait for termination signal (Ctrl+C)
-	<-signals
+func RunTopic1Consumer(consumer *utils.KafkaConsumer) {
+	var wg sync.WaitGroup
 
-	// Close the consumer gracefully
+	kafkaConsumer := consumer.GetConsumer()
+	var signals = make(chan os.Signal, 1)
+
+	signal.Notify(signals, os.Interrupt)
+	
+	go StartConsumer(kafkaConsumer,&wg)
+
+	select {
+	case <-signals :
+		fmt.Println("Signal for termination received , sending message to in temp_signal ")
+		temp_signal<-true
+	}
+
 	consumer.CloseConsumer()
-
+	
 	wg.Wait()
+	
 }
 
 func StopAndClose(consumer *utils.KafkaConsumer) {
-	// Close the Kafka consumer gracefully
 	consumer.CloseConsumer()
 }
--- central-broker/main.go
+++ central-broker/main.go
@@ -10,40 +10,25 @@ import (
 	"github.com/geekyabhi/resource-allocater/utils"
 )
 
-func main() {
-	// Initialize Kafka consumers for your topics
-	topic1Consumer := consumers.NewTopic1Consumer()
-	// Add more consumers for other topics if needed
+func runConsumer(consumer *utils.KafkaConsumer , wg *sync.WaitGroup , consumerFunc func(consumer *utils.KafkaConsumer)){
+	wg.Add(1)
+	defer wg.Done()
+	consumerFunc(consumer)
+}
 
-	// Create a wait group to wait for all consumers to finish
+func main() {
 	var wg sync.WaitGroup
 
-	// Define a function to run a consumer and wait for it to finish
-	runConsumer := func(consumer *utils.KafkaConsumer) {
-		defer wg.Done()
-		consumers.RunTopic1Consumer(consumer) // Change to the appropriate Run function for each topic
-	}
+	topic1Consumer := consumers.NewTopic1Consumer()
 
-	// Start your Kafka consumers
-	wg.Add(1) // Add one for each consumer
-	go runConsumer(topic1Consumer)
-	// Start more consumers for other topics if needed
 
-	// Wait for termination signals
+	go runConsumer(topic1Consumer , &wg , consumers.RunTopic1Consumer)
+
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt)
 
-	// Wait for all consumers to finish or until a termination signal is received
-	select {
-	case <-signals:
-		fmt.Println("Received termination signal. Stopping consumers...")
-	}
+	<-signals
 
-	// Close and stop all consumers gracefully
-	consumers.StopAndClose(topic1Consumer)
-	// Stop and close more consumers for other topics if needed
-
-	// Wait for all consumers to finish
 	wg.Wait()
 
 	fmt.Println("All consumers have stopped.")
